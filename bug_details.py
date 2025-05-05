a0970f709a0c1244d41896579d24e325b92753df
Kostya Egorov <kosta.work0@gmail.com>
2025-05-05 17:45:48 +0300

    add

---
+++ bug_details.py
@@ -0,0 +1,15 @@
+# 7. Изменяемые и неизменяемые типы данных
+def bug_info():
+    id = int(input('введите id бага: '))
+    name = input('введите имя бага: ')
+    status = input('введите статус бага: ')
+
+    bug = {
+        'ID': id,
+        'name': name,
+        'status': status
+    }
+
+    return bug
+
+print(bug_info())
