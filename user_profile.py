317c38e737dc2e212ee8803c46ed55ffa00fce65
Kostya Egorov <kosta.work0@gmail.com>
2025-05-05 13:19:46 +0300

    hw user_profile.py

---
+++ user_profile.py
@@ -0,0 +1,16 @@
+# 4. Переменные
+while True:
+    name = input("Введите ваше имя: ")
+    if name:
+        break
+
+while True:
+    profession = input("Введите вашу профессию: ")
+    if profession:
+        break
+
+while True:
+    tool = input("Введите ваш любимый инструмент для тестирования: ")
+    break
+
+print(f"Ваш любимый инструмент: {tool}. Отличный выбор!")
