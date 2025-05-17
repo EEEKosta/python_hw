3d89a8f2fc9cc64d5a229682f5cfe86060e0a770
Kostya Egorov <kosta.work0@gmail.com>
2025-05-17 09:32:27 +0300

    hw

--- module_4_2_task_1/test_create_booking.py
+++ module_4_2_task_1/test_create_booking.py
@@ -23,7 +23,7 @@ class TestBooking:
         get_booking = auth_session.get(f"{BASE_URL}/booking/{booking_id}")
         assert get_booking.status_code == 404, "Бронь не удалена"
 
-
+    # проверка обновления брони
     def test_update_booking(self, auth_session, booking_data, booking_data_2):
         create_booking = auth_session.post(f"{BASE_URL}/booking", json=booking_data)
         assert create_booking.status_code == 200, "Ошибка при создании брони"
@@ -34,7 +34,7 @@ class TestBooking:
         update_booking = auth_session.put(f"{BASE_URL}/booking/{booking_id}", json=booking_data_2)
         assert create_booking.json()["booking"]["firstname"] != update_booking.json()["firstname"]
 
-
+    # проверка того что список additionalneeds добавилс
     def test_update_booking_additionalneeds(self, auth_session, booking_data, booking_data_2):
         create_booking = auth_session.post(f"{BASE_URL}/booking", json=booking_data)
         assert create_booking.status_code == 200, "Ошибка при создании брони"
@@ -45,7 +45,7 @@ class TestBooking:
         update_booking = auth_session.put(f"{BASE_URL}/booking/{boking_id}", json=booking_data_2)
         assert update_booking.json()["additionalneeds"] is not None, "Список отсутствует"
 
-
+    # проверка частичного изменения, одного поля
     def test_partial_update_booking(self, auth_session, create_booking, booking_data, booking_data_2):
         booking = create_booking
         booking_id = booking.get("bookingid")
@@ -54,7 +54,7 @@ class TestBooking:
         update_booking = auth_session.patch(f"{BASE_URL}/booking/{booking_id}", json={"firstname": faker.first_name()})
         assert booking["booking"].get('firstname') != update_booking.json().get("firstname")
 
-
+    # проверка того что созданая бронь есть в общем списке
     def test_get_booking(self, auth_session, booking_data):
         create_booking = auth_session.post(f"{BASE_URL}/booking", json=booking_data)
         assert create_booking.status_code == 200, "Ошибка при создании брони"
@@ -63,7 +63,7 @@ class TestBooking:
         get_booking = auth_session.get(f"{BASE_URL}/booking")
         assert any(item["bookingid"] == booking_id for item in get_booking.json()), "Бронь не найдена"
 
-
+    # проверка json schema
     def test_get_booking_json(self, auth_session, booking_data):
         response = auth_session.get(f"{BASE_URL}/booking")
         assert response.status_code == 200, "Ошибка при получении списка броней"
